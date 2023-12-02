4da41250628c9ab8ace19e9083c9fcf43b33c40c
sivaguru-mediwave <sivaguru@mindwaveventures.com>
2023-12-03 00:55:24 +0530

    database and sequelize setup completed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import app from './app';
 import dotenv from 'dotenv';
 import { logger } from './helpers/logger';
+import { sequelize } from './database/connection';
 
 dotenv.config();
 
 const port = process.env.PORT;
+
+sequelize
+  .authenticate()
+  .then(async () => {
+    logger.debug('Database Connected');
+  })
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   logger.info(`Applications running on port ${port}`);
